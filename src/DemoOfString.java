5ec5a38d41419dd63efb8873d5415f617c59091f
Divyanshu Singh <divyanshu.singh_cs.aiml19@gla.ac.in>
2020-08-04 18:03:14 +0530

    Initial commit

---
+++ src/DemoOfString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DemoOfString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+        System.out.println(inputString);
+
+    }
+
+}
