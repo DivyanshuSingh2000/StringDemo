120b29be6f98daa19e5c5aa81d552f8586a7e425
Divyanshu Singh <divyanshu.singh_cs.aiml19@gla.ac.in>
2020-08-04 21:18:56 +0530

    task5 completed!

--- src/DemoOfString.java
+++ src/DemoOfString.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DemoOfString {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String inputString = sc.nextLine();
-        System.out.println(inputString);
+        String inputString = new String();
+        inputString = sc.nextLine();
+        System.out.print("index: ");
+        for (int i = 0; i < inputString.length(); i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.print("\nchars: ");
+        for( int j = 0; j < inputString.length(); j++){
+            System.out.print(inputString.charAt(j) + "\t");
+        }
+        }
 
     }
-
-}
